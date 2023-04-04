19f9a935b92a8c121854cdf39f0ef90d77b4c967
JMSBRN <zakhavai@gmail.com>
2023-04-04 20:06:35 +0300

    fix: updated css for reset filter btn

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -1,52 +1,49 @@
-import React, { useState }  from "react";
+import React from "react";
 import { Task } from "../../interfaces/appInterfaces";
 
 interface TagsProps {
   tasks: Task[];
-  setTasks: React.Dispatch<React.SetStateAction<Task[]>>
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
   tags: string[];
   onClick: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 const Tags = (props: TagsProps) => {
   const { tags, onClick, setTasks, tasks } = props;
-  const [isActive, setIsActive] = useState(false);
-  const handleSetAllTasks = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleSetAllTasks = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     if (e.currentTarget.parentNode) {
       const childElements = e.currentTarget.parentNode.querySelectorAll(".tag");
       if (childElements) {
         childElements.forEach((child) => {
-          if (isActive) {
-            child.classList.add("active");
-          } else {
-            child.classList.remove("active");
-          }
+          child.className = 'tag';
         });
-        setIsActive(!isActive);
       }
     }
-    
-    setTasks(tasks.map( el => {
-      return { ...el, hidden: !el.hidden }
-    }))
+
+    setTasks(
+      tasks.map((el) => {
+        return { ...el, hidden: false };
+      })
+    );
   };
-  
+
   return (
-    <>
+    <div className="tags-container">
       <div className="tags">
-        <button onClick={(e) => handleSetAllTasks(e)}>all tasks</button>
-        {
-          tags.map((el, idx) => (
-            <div
-              id={el}
-              key={idx.toString()}
-              className="tag"
-              onClick={(e) => onClick(e)}
-            >
-              {el}
-            </div>
-          ))}
+        {tags.map((el, idx) => (
+          <div
+            id={el}
+            key={idx.toString()}
+            className="tag"
+            onClick={(e) => onClick(e)}
+          >
+            {el}
+          </div>
+        ))}
       </div>
-    </>
+         <button onClick={(e) => handleSetAllTasks(e)}>reset tag filter</button>
+    </div>
   );
 };
 
