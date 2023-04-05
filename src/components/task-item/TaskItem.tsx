6b19f272c5cdb44e270d1649b13d09ad1a9eb15f
JMSBRN <zakhavai@gmail.com>
2023-04-05 17:49:05 +0300

    fix: update TaskItem (relocate edit button)

--- src/components/task-item/TaskItem.tsx
+++ src/components/task-item/TaskItem.tsx
@@ -1,35 +1,42 @@
-import React from 'react';
 import { Task } from '../../interfaces/appInterfaces';
-
 
 interface TaskProps {
   idx: number;
-  el:Task;
+  el: Task;
   input: string;
-  handleGetText:(text: string, id: string) => void;
-  handleDeleteTask:  (id: string) => void;
+  handleGetText: (text: string, id: string) => void;
+  handleDeleteTask: (id: string) => void;
   handleEditTask: (text: string, id: string) => void;
 }
 const TaskItem = (props: TaskProps) => {
-    const {idx, el, input, handleDeleteTask, handleEditTask, handleGetText } = props;
+  const { idx, el, input, handleDeleteTask, handleEditTask, handleGetText } =
+    props;
   return (
-    <div id={el.id} className="task" style={{ display:  el.hidden ? 'none' : 'flex' }} key={el.id}>
-    <span>{+(idx + 1)}</span>
     <div
-      className="task-text"
-      onClick={() => handleGetText(el.text, el.id)}
+      id={el.id}
+      className="task"
+      style={{ display: el.hidden ? 'none' : 'flex' }}
+      key={el.id}
     >
-      {<p dangerouslySetInnerHTML={{ __html: el.text }}></p>}
+      <span>{+(idx + 1)}</span>
+      <div className="task-text">
+        {<p dangerouslySetInnerHTML={{ __html: el.text }}></p>}
+      </div>
+      <div className="btns">
+        {el.update && (
+          <button
+            className="btn-update"
+            onClick={() => handleEditTask(input, el.id)}
+          >
+            update
+          </button>
+        )}
+        <button onClick={() => handleDeleteTask(el.id)}>delete</button>
+      </div>
+      <div className="edit" onClick={() => handleGetText(el.text, el.id)}>
+        edit
+      </div>
     </div>
-    <div className="btns">
-      {el.update && (
-        <button className='btn-update' onClick={() => handleEditTask(input, el.id)}>
-          update
-        </button>
-      )}
-      <button onClick={() => handleDeleteTask(el.id)}>delete</button>
-    </div>
-  </div>
   );
 };
 
