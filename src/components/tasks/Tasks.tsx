362530ecddb2eae34829c99fa45e68803cfd3b7c
JMSBRN <zakhavai@gmail.com>
2023-04-05 19:55:32 +0300

    fix: created start note

--- src/components/tasks/Tasks.tsx
+++ src/components/tasks/Tasks.tsx
@@ -48,6 +48,41 @@ const Tasks = (props: TasksProps) => {
             />
           </Reorder.Item>
         ))}
+        {!tasks.length && (
+          <div className="start-note">
+            <h4>Start Note!</h4>
+            <p>
+              To create a tag while editing a note, type "#" followed by the tag
+              name within the note text field. For example, "I want to go to
+              #home". When you finish typing the tag name and click the New task
+              button, the corresponding tag will be automatically created and
+              displayed in the tag list below the text field.
+            </p>
+
+            <p>
+              To edit a tag, click on the task in the edit field in the list and
+              make the desired changes. Then click the update button.
+            </p>
+
+            <p>
+              While editing a note, all words that match the tags in the tag
+              list will be automatically highlighted for easy identification.
+            </p>
+
+            <p>
+              To sort tasks by drag and drop, click and hold on a task, and drag
+              it to a new position in the list. The updated order will be
+              automatically saved and reflected in the task list.
+            </p>
+
+            <p>
+              <h4>Note!</h4>
+              That you can add multiple tags to a note by using "#"
+              followed by the tag name, separated by spaces. For example, "#shop
+              #groceries #errands".
+            </p>
+          </div>
+        )}
       </div>
     </Reorder.Group>
   );
